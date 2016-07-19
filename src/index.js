80e0befe5acea2b876d5ab70ef4d5252702289b5
RichAbrahams <richardabrahams75@gmail.com>
2016-07-19 03:29:06 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// Set up your application entry point here...
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/styles.styl';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {newGrid} from './logic/gridFunctions';
+
+const initialGrid = newGrid(10,10);
+const initialState = {
+  grid: initialGrid,
+  rows: 10,
+  columns: 10,
+  run: true,
+  generations: 0
+};
+const store = configureStore(initialState);
+
+/*
+store.dispatch(loadStore());
+componentWillMount (){
+    let p = newGrid(10,10);
+    console.log('grid', p);
+    let x = nextGrid(p);
+    console.log('nextGrid', x);
+}
+*/
+
+render (
+  <Provider store={store}>
+  <Router history={browserHistory} routes={routes} />
+  </Provider>
+  ,  document.getElementById('app')
+);
