b5ffb82cb0ad5dfd3ef3cd5a2fe36d5afe3462af
RichAbrahams <richardabrahams75@gmail.com>
2016-07-19 04:16:33 +0100

    actions, reducers edited

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
 // Set up your root reducer here...
  import { combineReducers } from 'redux';
- export default combineReducers;
+ import gridReducer from './gridReducer';
+
+ const rootReducer = combineReducers({
+   grid: gridReducer,
+   rows: gridReducer,
+   columns: gridReducer,
+   run: gridReducer,
+   generations: gridReducer
+ });
+
+ export default rootReducer;
